c6a71eb581d102ae97992abafe565e4a70d44280
athaka <jesoka18@gmail.com>
2023-11-14 18:03:14 +0100

    tough times, tough skin

---
+++ _printf.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _printf - A function that produces output according to a format.
+ * @format: A pointer to the desired output format.
+ *
+ * Return: The number of characters printed (excluding
+ * the null byte used to end output to strings).
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			count++;
+		}
+		else
+		{
+			format++;
+			if (*format == '\0')
+				break;
+			switch (*format)
+			{
+				case 'c':
+					count += print_char(args);
+					break;
+				case 's':
+					count += print_str(args);
+					break;
+				case '%':
+					count += print_percent(args);
+					break;
+			}
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
+}
--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
+#include <unistd.h>
+
 int _printf(const char *format, ...);
 
-#endif
+/* Function prototypes for format specifiers */
+int print_char(va_list args);
+int print_str(va_list args);
+int print_percent(va_list args);
+
+#endif /* MAIN_H */
